7d2cc652dd6448c03d9a0603c8526794d90d3491
GabrielDevJourney <gabspereira178@gmail.com>
2024-01-30 09:39:14 +0000

    getting all elements from html, creating a class calculator, define a object calculator trough that same class

--- code/script.js
+++ code/script.js
@@ -1,78 +1,55 @@
-//*GETTING ALL THE NEEDED NUMBERS
-//*function to display the opetaration
-
-let display = document.querySelector('#display');
-let currentInput = '';
-
-function appendNumber(num){
-  currentInput += num;
-  display.value = currentInput;
-}
-
-function appendOperator(operator) {
-  currentInput += operator;
-  display.value = currentInput;
-}
-
-function clearDisplay(){
-  //*CLEAN BOTH VARIABLE STORING VALUE AND DISPLAY VALUE
-  currentInput = '';
-  display.value = '';
-
-}
-
-
-function add(num1,num2){
-  return num1 + num2;
-}
-function subtract(num1, num2){
-  return num1 - num2;
-}
-
-function multiply(num1, num2){
-  return num1 * num2;
-}
-
-function divide(num1, num2){
-
-  if (b === 0) {
-
-    console.error("Cannot divide by zero");
-    return undefined; // i will see if i return a error or something
+class Calculator{
+  constructor(previousOperandTextElement, currentOperandTextElement){
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    //*WHEN WE CALL CALCULATOR ALL INPUT WILL BE CLEAR
+    this.clear()
   }
-  return num1 / num2;
+
+  //*CLEAR ALL DISPLAY
+  clear(){
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+
+  //*DELETE SINGLE CHARACATERS
+  delete(){
+
+  }
+
+  //*CLICK NUMBER AND ADD TO DISPLAY
+  appendNumber(number){
+
+  }
+
+  chooseOperation(operation){
+
+  }
+
+  //*DO THE MATH TO COMPUTE A SINGLE VALUE
+  compute(){
+
+  }
+
+  //*UPDATE VALUES IN THE OUTPUT
+  update(){
+
+  }
 }
 
-let number1;
-let number2;
-let operators;
 
-function operate(operator, numb1, numb2){
-  currentInput = currentInput.toString();
-  console.log(currentInput);
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operatio]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector("[data-delete]")
+const allClearButton = document.querySelector("[data-all-clear]")
+const previousOperandTextElement = document.querySelector("[data-previous-operand]")
+const currentOperandTextElement = document.querySelector("[data-current-operand]")
 
-  switch (operator) {
-    case '+' :
+//*DEFINE CLASS
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-      return add(numb1,numb2);
-
-    case '-' :
-
-      return subtract(numb1,numb2);
-
-      case '*' :
-
-      return multiply(numb1,numb2);
-
-      case '/' :
-
-      return divide(numb1,numb2);
-
-      default:
-
-      console.error('Invalid Operator');
-      return undefined //*for now return undefined
-
-    }
-}
-
+numberButtons.forEach(button => {
+  
+})
