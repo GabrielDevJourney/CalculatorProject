71e6e5e230a7e12b03f3d0cfe51f58f912ff6219
GabrielDevJourney <gabspereira178@gmail.com>
2024-01-30 10:01:23 +0000

    append number to display update the display and when operator is choosen the previusoperand display is updated with the currtent so this can then be clean

--- code/script.js
+++ code/script.js
@@ -19,12 +19,20 @@ class Calculator{
   }
 
   //*CLICK NUMBER AND ADD TO DISPLAY
+  //*DO NOT LET MULTIPLE DOTS TO BE USED
   appendNumber(number){
-
+    if(number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
   }
 
   chooseOperation(operation){
-
+    //*dont let return if there is no numbers
+    if(this.currentOperand === '') return
+    this.operation = operation
+    //*set the display operand to the current 
+    this.previousOperand = this.currentOperand
+    //*clean the current
+    this.currentOperand = ''
   }
 
   //*DO THE MATH TO COMPUTE A SINGLE VALUE
@@ -33,8 +41,10 @@ class Calculator{
   }
 
   //*UPDATE VALUES IN THE OUTPUT
-  update(){
-
+  updateDisplay(){
+    this.currentOperandTextElement.innerText = this.currentOperand
+    //*display the current to the display that show all operands
+    this.previousOperandTextElement = this.previousOperand
   }
 }
 
@@ -50,6 +60,17 @@ const currentOperandTextElement = document.querySelector("[data-current-operand]
 //*DEFINE CLASS
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
+//*WHEN NUMBER CLICKED APPEND IT AND UPDATE IT TO DISPLAY
 numberButtons.forEach(button => {
-  
-})
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+});
+
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
